daad51d48829f8417f41436cba6ad7e6bb8c23ca
gustavo9601 <tavo9601@gmail.com>
2020-07-28 00:20:09 -0500

    Añadiendo modal donacion 2

---
+++ views/modal-donacion2.php
@@ -0,0 +1,56 @@
+<div class="modal fade" tabindex="-1" role="dialog" id="modal-donacion">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header" style="border: none">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-footer" style="border: none">
+
+                <div class="row m0 comment_form">
+                    <h3 style="text-align: center">APOYA EL PROYECTO AXITECH DONACION 22222</h3>
+                    <p style="text-align: center">
+                        Portal web de divulgacion cientifica, proyectos o de publicaciones de universidades, resultados de tesis de investigaciones, explicadas por sus autores
+                        <a href="#"  id="ver-terminos-condiciones" class="btn btn-xs">
+                            <i class="fa fa-globe"></i>
+                            Leer mas
+                        </a>
+                    </p>
+
+                    <form action="#" class="row m0 commentForm">
+                        <div class="input-group">
+                            <input type="number" class="form-control" placeholder="Identificacion o NIT">
+                            <span class="input-group-addon"><i class="fa fa-male"></i></span>
+                        </div>
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Nombre">
+                            <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                        </div>
+
+                        <div class="input-group">
+                            <input type="email" class="form-control" placeholder="Email">
+                            <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                        </div>
+                        <div class="input-group">
+                            <input type="number" class="form-control" placeholder="Donacion" min="10000" value="10000">
+                            <span class="input-group-addon"  ><i class="fa fa-money"></i></span>
+                        </div>
+                        <div class="row m0" style="text-align: center">
+                            <label for="terminos-condiciones">
+                                <input type="checkbox" name="" id="terminos-condiciones">
+                                <small>
+                                    Acepta nuestras condiciones de uso y politicas de privacidad
+                                </small>
+                            </label>
+                        </div>
+                        <div class="row m0" style="text-align: center">
+                            <!-- <input type="checkbox" id="notify" class="hidden">-->
+                            <!-- <label for="notify" class="notify">Notify me when new comments are posted.</label>-->
+                            <input type="submit" value="REALIZAR DONACION" class="btn" style="float:none; text-align: center">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
